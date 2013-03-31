faddd2725340a885d015e81a63aab4777b33ff40
Kyle Reicks <reicks.kyle@gmail.com>
2013-03-30 22:22:11 -0500

    Queue scripts up by dependency and make seperate calls to head.js when the execution order is inconsequential.

--- head-js-wp.php
+++ head-js-wp.php
@@ -30,6 +30,8 @@ if(!class_exists('Head_js_wp')){
     function print_scripts_with_header_js(){
       global $wp_scripts;
 
+      $script_trees = array();
+
       print_late_styles();
 
       if(!empty($wp_scripts->queue)){
@@ -42,20 +44,51 @@ if(!class_exists('Head_js_wp')){
         foreach($wp_scripts->to_do as $handle){
           $wp_scripts->print_extra_script($handle);
         }
-        
-        echo '<script>' . "\n" . 'head.js({';
-        foreach($wp_scripts->to_do as $script){
-          $ending = (count($wp_scripts->to_do) === 1) ? ' });' : " },\n{";
-          $version = ($wp_scripts->registered[$script]->ver) ? '?ver=' . $wp_scripts->registered[$script]->ver : "";
-          $base_url = (preg_match('/^\//', $wp_scripts->registered[$script]->src)) ? $wp_scripts->base_url : '';
-          echo str_replace('-', '_', $script) . " : '" . $base_url . $wp_scripts->registered[$script]->src . $version . "'" . $ending;
-          $wp_scripts->done[] = $script;
-          $wp_scripts->to_do = array_merge(array_diff($wp_scripts->to_do, array($script)));
-        }
-        echo '</script>' . "\n";
-      }
 
-      return true;
+        foreach($wp_scripts->to_do as $script){
+          if(empty($wp_scripts->registered[$script]->deps)){
+            $script_trees[] = array(
+              $script
+            );
+          }else{
+            foreach($script_trees as $number => $tree){
+              foreach($tree as $tree_script){
+                if(in_array($tree_script, $wp_scripts->registered[$script]->deps) && !in_array($script, $script_trees[$number])){
+                  $script_trees[$number][] = $script;
+                }
+              }
+            }
+          }
+        }
+
+        foreach($script_trees as  $number => $tree){
+          if(isset($script_trees[$number]) && isset($script_trees[$number + 1])){
+            $intersect = array_intersect($script_trees[$number], $script_trees[$number +1]);
+            if(!empty($intersect)){
+              $script_trees[$number] = array_diff($script_trees[$number], $intersect);
+              $script_trees[$number] = array_merge($script_trees[$number], $script_trees[$number + 1]);
+              unset($script_trees[$number + 1]);
+            }
+          }
+        }
+
+        echo '<script>' . "\n" . 'head';
+        foreach($script_trees as $tree){
+          echo '.js({';
+          foreach($tree as $number => $script){
+            $ending = (count($tree) === $number + 1) ? ' })' : " },\n{";
+            $version = ($wp_scripts->registered[$script]->ver) ? '?ver=' . $wp_scripts->registered[$script]->ver : "";
+            $base_url = (preg_match('/^\//', $wp_scripts->registered[$script]->src)) ? $wp_scripts->base_url : '';
+            echo str_replace('-', '_', $script) . " : '" . $base_url . $wp_scripts->registered[$script]->src . $version . "'" . $ending;
+            $wp_scripts->done[] = $script;
+            $wp_scripts->to_do = array_merge(array_diff($wp_scripts->to_do, array($script)));
+          }
+        }
+        echo ';' . "\n" . '</script>' . "\n";
+        return true;
+      }else{
+        return false;
+      }
     }
   }/* End class Head_js_wp */
   $head_js_wp = new Head_js_wp();
