e2775b85cc88274a3b9188958636a0f577212452
Kyle Reicks <reicks.kyle@gmail.com>
2013-09-15 11:34:48 -0500

    Add head loader version constant

--- head-js-wp.php
+++ head-js-wp.php
@@ -11,6 +11,7 @@ Author URI: http://github.com/kylereicks/
 define('HEAD_JS_WP_PATH', plugin_dir_path(__FILE__));
 define('HEAD_JS_WP_URL', plugins_url('/', __FILE__));
 define('HEAD_JS_WP_VERSION', '1.0.0');
+define('HEAD_JS_VERSION', '0.99');
 
 require_once(HEAD_JS_WP_PATH . 'inc/class-head-js-wp.php');
 
